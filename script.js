ec5e04069ece30280dc1741659d7fa0c04f1eaef
lucasbeo <lucasbeo51@gmail.com>
2022-11-22 18:05:36 -0300

    Testando códigos, tentando avançar

--- script.js
+++ script.js
@@ -30,15 +30,50 @@ document.querySelector('#pressione').onclick = function(){
 
         document.querySelector("#novatarefa input").value = "";
     }
-    function Cadastrar(){
+    function cadastrar(){
       
         document.getElementById('tudo').style.display="block";
   
       }
-        function Exibir(){
+        function exibir(){
         
           document.getElementById('tudo').style.display="none";
           
   
+        }
+
+        function adicionar()
+        {
+            
+          var conteudo= document.getElementById('texto').value; 
+          if(conteudo!="") { var cont=1;
+          var novoElemento= document.createTextNode(conteudo);
+          var Novadiv= document.createElement('div');
+          Novadiv.style.position='relative';
+          Novadiv.style.backgroundColor='black';
+          Novadiv.style.borderRadius='5px';
+          Novadiv.style.color='white';
+          Novadiv.style.border='solid 2px';
+          Novadiv.style.borderColor='purple'; 
+        }
+     }
+  
+
+
+
+        function user(){
+
+            var ad = document.getElementById("ad");
+            var login = document.getElementById("login").value;
+            var nome = document.getElementById("nome").value;
+            var senha = document.getElementById("senha").value;
+            var verificar = document.getElementById("confirmarsenha").value;
+            var email = document.getElementById("email").value;
+
+            user.innerHTML= "Nome: "+ nome +'<br>'+ '<hr>'+
+            "Login: "+ login +'<br>'+ '<hr>'+
+            "Senha: "+ senha +'<br>'+'<hr>'+
+           "Email: "+ email ;
+        
         }
 }
